f4b995123c6e2fd03582e21e2f8343d9dbd512a1
Felipe Pérez Leal <seperezl@unal.edu.co>
2023-11-11 09:22:43 -0500

    (GET) All Flights added at ./flight/flights

--- Readme.txt
+++ Readme.txt
@@ -1,7 +1,10 @@
 docker pull mongo
-docker run -p 27017:27017 -v \data\db:"c:\Users\Cristian\Documents\Semestres\Semestre 2023-2\Arquisoft\Prototipos\Prototipo_Flights_1\data_db" --name databaseflights mongo
+docker run -d -p 27017:27017 -v D:\user\Documents\Arquisoft\fligth_ms\data_db:/data/db --name databaseflights mongo
 docker network create --attachable flight_network 
 docker network connect flight_network databaseflights 
+
 docker build -t flight_ms_1 . 
-docker run -it -p 5000:5000 --name flight_ms flight_ms_1 
-docker network connect flight_network "flight_ms"
+docker run -it -p 4999:5000 --network="flight_network" --name flight_ms flight_ms_1 
+
+docker build -t swarch2023i_ag .
+docker run -p 5000:5000 --name ap swarch2023i_ag
---
+++ Tripster_MS/src/app.py
@@ -0,0 +1,58 @@
+from flask import Flask, request,jsonify, Response
+from flask_pymongo import PyMongo
+from bson import json_util
+
+
+app=Flask(__name__) #Crea una instancia de una aplicacion
+app.config['MONGO_URI']='mongodb://databaseflights:27017/Tripster_Flights_Database'
+
+mongo=PyMongo(app) #Se obtiene coneccion a base de datos
+
+@app.route('/flight/country',methods=['POST'])#Creacion de rutas para añadir country (esta a la escuha)
+def create_country():
+    #Receiving country
+    country_name=request.json['country_name'] #Los datos del Json recibido se almacena en variables
+    if country_name:
+        id=mongo.db.Country.insert_one(
+            {'country_name':country_name}
+        )
+        response={
+            'message':str(id),
+            'country_name':country_name        
+        }
+        return response
+    else:
+        return not_found()
+@app.route('/flight/flight',methods=['Get'])
+def give_flights():
+    origin=request.json['origin']
+    destination=request.json['destination']
+    print("Hola Mundo!")
+    print(request.json)
+    if origin and destination:
+        #esta funcion obtiene los datos en formato Bson
+        flights=mongo.db.Flight.find({'$and':[{'airport_origin.airport_origin_name':{'$eq':origin}},{'airport_destination.airport_destino_name':{'$eq':destination}}]})
+        response=json_util.dumps(flights) # Aqui se pasa de Bson a Json
+        return Response(response,mimetype='application/json')
+
+@app.route('/flight/flights', methods=['GET'])
+def get_all_flights():
+    flights = mongo.db.Flight.find()  # Obtiene todos los vuelos de la base de datos
+    response = json_util.dumps(flights)  # Convierte los datos a formato JSON
+    print(response)
+    return Response(response, mimetype='application/json')
+
+
+@app.errorhandler(404) #Manejo de errores
+def not_found(error=None):
+    response=jsonify({
+        'message':'Resource Not Found:'+ request.url,
+        'status':404
+    })
+    response.status_code=404
+    return response
+    
+
+
+if __name__== "__main__":
+    app.run(host="0.0.0.0",port=5000,debug=True)
